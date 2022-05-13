b57860fb614caaff441389cab69235f1f4abea1f
Sergio Berdeja <sergioberdeja@gmail.com>
2022-05-13 17:39:21 -0600

    Completed assignmen

--- index.js
+++ index.js
@@ -21,3 +21,70 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    let empObj = {
+        'firstName':firstName, 
+        'familyName':familyName,
+        'title':title,
+        'payPerHour':payPerHour,
+        'timeInEvents':[],
+        'timeOutEvents':[]
+    }
+    return empObj
+}
+
+function createEmployeeRecords(records) {
+    return records.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(timeStamp) {
+    let dateArr = timeStamp.split(' ')
+    let timeInObj = {
+        'type':"TimeIn",
+        'hour': parseInt(dateArr[1].slice(0,2) + '00'),
+        'date': dateArr[0]
+    }
+    this.timeInEvents.push(timeInObj)
+    return this
+}
+
+function createTimeOutEvent(timeStamp) {
+    let dateArr = timeStamp.split(' ')
+    let timeOutObj = {
+        'type':"TimeOut",
+        'hour': parseInt(dateArr[1].slice(0,2) + '00'),
+        'date': dateArr[0]
+    }
+    this.timeOutEvents.push(timeOutObj)
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    let hourOut, hourIn
+    for (const out of this.timeOutEvents) {
+        if (out['date'] === date) {
+            hourOut = out['hour']
+        }
+    }
+    for (const inV of this.timeInEvents) {
+        if (inV['date'] === date) {
+            hourIn = inV['hour']
+        }
+    }
+    return (hourOut-hourIn)/100
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+function calculatePayroll(array) {
+    return array.reduce((acc, ele) => {
+        console.log(allWagesFor.call(ele))
+        return acc + allWagesFor.call(ele)
+    }, 0)
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(rec => rec.firstName === firstName)
+}
